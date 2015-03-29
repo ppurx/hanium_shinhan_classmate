4acf1fff502f4515c476f50c961a428f355e499b
sh <sh@sh-PC>
2015-03-29 14:45:57 +0900

    김상현 2015-03-29 com/MemeberDAO,MemberDTO,MemberProcess 커밋, 문제시 라이브러리 추가

--- hanium_shinhan_classmate/src/com/MemberProcess.java
+++ hanium_shinhan_classmate/src/com/MemberProcess.java
@@ -1,5 +1,57 @@
 package com;
 
-public class MemberProcess {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MemberProcess extends HttpServlet {
+	
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+			
+		String uri = request.getRequestURI();
+		System.out.println("uri : " + uri);
+		
+		String command = uri.substring(uri.lastIndexOf("/") + 1, uri.lastIndexOf(".kim"));
+		
+		if(command != null && command.trim().equals("register")){
+			
+			String num = request.getParameter("num");
+			String name = request.getParameter("name");
+			String tel = request.getParameter("tel");
+			String addr = request.getParameter("addr");
+			
+			MemberDTO dto = new MemberDTO();
+			dto.setNum(Integer.parseInt(num));
+			dto.setName(name);
+			dto.setTel(tel);
+			dto.setAddr(addr);
+			
+			MemberDAO dao = new MemberDAO();
+			boolean bool = dao.insertMember(dto);
+			
+			if(bool){
+				response.sendRedirect("Success.html");
+			} else
+			{
+				response.sendRedirect("Fail.html");
+			}
+			
+			
+			
+			
+		} else if (command.trim().equals("update")){
+			
+		}
+		
+		
+	}
 
 }
